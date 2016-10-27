8582ed3ed3c3cf13d844d128b28d3b035ccf1f96
wangjianqiang <376710653@qq.com>
2016-10-27 15:51:33 +0800

    提交试试

---
+++ AnnotationTest.java
@@ -0,0 +1,24 @@
+package annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Inherited
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD,ElementType.TYPE})
+public @interface AnnotationTest {
+	
+	String value() default "hello";
+	String value1() default "world";
+	
+}
+
+
+enum EnumTest{
+	Hello,World,Welcome;
+}
